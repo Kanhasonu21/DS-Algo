d1fc7fc2306d748d75e21414796ee54ef816aba3
Kanhaiya Kumar <kanhaiya.kumar@webdunia.net>
2021-05-18 23:33:22 +0530

    moveZerotoEnd complexity improved

--- moveZerotoEnd.js
+++ moveZerotoEnd.js
@@ -1,14 +1,16 @@
-let arr = [5, 6, 0, 7, 0, 1, 2, 0];
+let arr = [5, 6, 0, 0, 0, 7, 0, 1, 2, 0];
+function swap(ele1, ele2) {
+  let temp;
+  (temp = ele1), (ele1 = ele2), (ele2 = temp);
+  return ele1, ele2;
+}
 const moveZero = (arr) => {
   let res = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== 0) {
-      arr[res] = arr[i];
+      [arr[i], arr[res]] = [arr[res],arr[i]]
       res++;
     }
-  }
-  for (let i = res; i < arr.length; i++) {
-    arr[i] = 0;
   }
   console.log(arr);
 };
