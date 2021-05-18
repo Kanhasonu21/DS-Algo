b32029a11caaae9c2c65d381ec6b2b0df6634913
Kanhaiya Kumar <kanhaiya.kumar@webdunia.net>
2021-05-18 23:04:04 +0530

    improved complexity on duplicate array

--- removeduplicateSorted.js
+++ removeduplicateSorted.js
@@ -1,13 +1,24 @@
-const arr = [10, 20, 20, 30, 30, 30, 40, 40, 40, 50];
+// const arr = [10, 20, 20, 30, 30, 30, 40, 40, 40, 50];
+// const removeDuplicate = (arr) => {
+//   let res = 0;
+//   let newArr = [arr[0]];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] !== newArr[res]) {
+//       newArr.push(arr[i]);
+//       res += 1;
+//     }
+//   }
+//   console.log(newArr);
+// };
+let arr = [10, 20, 20, 30, 30, 30, 40, 40, 40, 50];
 const removeDuplicate = (arr) => {
-  let res = 0;
-  let newArr = [arr[0]];
+  let res = 1;
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== newArr[res]) {
-      newArr.push(arr[i]);
-      res += 1;
+    if (arr[i] !== arr[i - 1]) {
+      arr[res] = arr[i];
+      res++;
     }
   }
-  console.log(newArr);
+  for (let i = 0; i < res; i++) console.log(arr[i]);
 };
 removeDuplicate(arr);
