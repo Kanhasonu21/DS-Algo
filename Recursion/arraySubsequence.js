3da0aaa181bd96f2352e1799abd69ecf320f8ae6
Kanhasonu21 <kanhaiyak42@gmail.com>
2022-02-02 00:07:39 +0530

    sum of subsequence added

--- Recursion/arraySubsequence.js
+++ Recursion/arraySubsequence.js
@@ -14,3 +14,29 @@ function subsequnce(ind, temp, size) {
   subsequnce(ind + 1, temp, size);
 }
 subsequnce(0, [], size);
+
+//sum matching subsequence
+
+
+let arr = [3, 1, 2];
+let size = arr.length;
+let temp = [];
+let sum = 3;
+function subsequnce(ind, temp, currentSum) {
+  if (ind >= size) {
+    if (currentSum === sum) {
+      console.log(temp);
+    }
+    // console.log(temp);
+
+    return;
+  }
+  //take
+  currentSum += arr[ind];
+  temp.push(arr[ind]);
+  subsequnce(ind + 1, temp, currentSum);
+  currentSum -= temp.pop();
+  //not take
+  subsequnce(ind + 1, temp, currentSum);
+}
+subsequnce(0, [], 0);
